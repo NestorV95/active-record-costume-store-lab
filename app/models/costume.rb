0b6396d2b5dc4c664759de3e41dffa493adc4072
Nestor <nestor.venegas95@gmail.com>
2021-03-08 21:48:49 -0600

    commit tree 671b2fcabdb1343eadbbd6a1bf5cb8486051e0e9

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,15 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class CreateCostumes < ActiveRecord::Base
+
+    def change
+        create_table :costumes do |t|
+        t.string :name
+        t.float :price
+        t.string :size
+        t.text :image_url
+    
+    end
+
+end
